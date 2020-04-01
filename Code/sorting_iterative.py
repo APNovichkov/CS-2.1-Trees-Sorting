17e412398938ff827947ccd2f9e719b89075c5c3
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-31 21:37:36 -0700

    [random]

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -1,4 +1,5 @@
 #!python3
+from random import shuffle
 
 
 def is_sorted(items):
@@ -25,9 +26,12 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
 
     Time Complexity: O(n^2) worst case running time when array is completely reversed. Generally still quadratic...
-    Memory Usage: We perform swapping, so everything happens in place. Only memory used is for the 3 variables
+    Memory Usage: We perform swapping, so everything happens in place. Only memory used is for some of the variables
 
     """
+
+    if len(items) == 0:
+        return []
 
     limit = len(items) - 1  # set upper limit to array size
     continueSort = True
@@ -36,15 +40,14 @@ def bubble_sort(items):
         continueSort = False  # Set default to false for each run through array
         for i in range(limit):  # Run through all elements until the upper limit
             if items[i] > items[i + 1]:
-                tmp = items[i]  # Swap elements here
-                items[i] = items[i + 1]
-                items[i + 1] = tmp
+                _swap(items, i, i + 1)
                 continueSort = True  # If there has been at least one swap, we should continue sorting..
         limit -= 1
 
     return items
 
 
+# TODO fix bug!
 def selection_sort(items):
     """
 
@@ -52,9 +55,12 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
 
     Running time: O(n^2) average and worst case. Worst case when array is completely reversed
-    Memory usage: Just the 2 variables, we are swapping again so no extra memory usage
+    Memory usage: Just some variables, we are swapping again so no extra memory usage
 
     """
+
+    if len(items) == 0:
+        return []
 
     limit = 0
     isSorted = False
@@ -66,12 +72,11 @@ def selection_sort(items):
         for i in range(limit + 1, len(items)):  # Loop from limit+1 to the end of array to find minIndex
             if items[i - 1] < items[i]:
                 isSorted = False
+                # print('isSortedIsFalse @ index: {}'.format(i))
             if items[i] < minValue:
                 minIndex = i
                 minValue = items[i]
-        tmp = items[limit]
-        items[limit] = items[minIndex]
-        items[minIndex] = tmp
+        _swap(items, minIndex, limit)
         limit += 1
 
     return items
@@ -83,23 +88,43 @@ def insertion_sort(items):
     Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
 
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?
+    Running time: O(n^2) average and worst case. Worst case when array is completely reversed
+    Memory usage: Just some variables, we are swapping again so no extra memory usage
 
     """
 
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Take first unsorted item
-    # TODO: Insert it in sorted order in front of items
+    if len(items) == 0:
+        return []
+
+    limit = 1
+
+    for i in range(1, len(items)):
+        # Reverse bubble sort
+        for j in range(0, limit):
+            lo_i = limit - j
+            hi_i = limit - j - 1
+            if items[lo_i] < items[hi_i]:
+                _swap(items, lo_i, hi_i)
+
+        limit += 1
+
+    return items
+
 
 def _swap(items, i, j):
-    pass
+    tmp = items[i]
+    items[i] = items[j]
+    items[j] = tmp
 
 
 if __name__ == '__main__':
-    items = [4, 5, 2, 3, 6, 1, 10, 2, 4]
-    print("Items before sorting: {}".format(items))
+    items = [i for i in range(50)]
+    shuffle(items)
+    # print("Items before sorting: {}".format(items))
     print(f"Is sorted? {is_sorted(items)}")
     # print("Items after Bubble Sort: {}".format(bubble_sort(items)))
-    print("Items after Selection Sort: {}".format(selection_sort(items)))
+    print(f"Is sorted? {is_sorted(bubble_sort(items))}")
+    # print("Items after Selection Sort: {}".format(selection_sort(items)))
     print(f"Is sorted? {is_sorted(selection_sort(items))}")
+    # print("Items after Insertion Sort: {}".format(insertion_sort(items)))
+    print(f"Is sorted? {is_sorted(insertion_sort(items))}")
