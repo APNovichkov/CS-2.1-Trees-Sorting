79e6df04181c2251c2c3e51ba120c6cc35afaad7
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-14 16:03:27 -0700

    [add] Added counting sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -12,6 +12,20 @@ def counting_sort(numbers):
     # TODO: Loop over counts and append that many numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
 
+    max_num = max(numbers)
+    min_num = min(numbers)
+    c_arr = [0] * (max_num - min_num + 1)
+
+    for num in numbers:
+        c_arr[num - min_num] += 1
+
+    numbers_index = 0
+    for index, counts in enumerate(c_arr):
+        val = index + min_num
+        for i in range(0, counts):
+            numbers[numbers_index] = val
+            numbers_index += 1
+
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
@@ -24,3 +38,10 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+
+
+if __name__ == '__main__':
+    l1 = [1, 3, 4, 2, 4, 1, 10]
+    print(f'before: {l1}')
+    counting_sort(l1)
+    print(f'after: {l1}')
