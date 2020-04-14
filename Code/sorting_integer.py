710eeb1f9d7aaadf7981c972e1379c6fba7c9412
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-14 16:54:58 -0700

    [add] Added bucket sort implementations

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -4,13 +4,9 @@
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
+
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
 
     max_num = max(numbers)
     min_num = min(numbers)
@@ -30,18 +26,35 @@ def counting_sort(numbers):
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
+
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+
+    max_num = max(numbers)
+    buckets = [[] for i in range(num_buckets)]
+
+    for num in numbers:
+        b_idx = _get_bucket_index(num, num_buckets, max_num)
+        buckets[b_idx].append(num)
+        buckets[b_idx].sort()
+
+    number_index = 0
+    for bucket in buckets:
+        for num in bucket:
+            numbers[number_index] = num
+            number_index += 1
+
+def _get_bucket_index(num, num_buckets, max_num):
+    return (num * num_buckets) // (max_num + 1)
 
 
 if __name__ == '__main__':
-    l1 = [1, 3, 4, 2, 4, 1, 10]
-    print(f'before: {l1}')
-    counting_sort(l1)
-    print(f'after: {l1}')
+    # l1 = [1, 3, 4, 2, 4, 1, 10]
+    # print(f'before: {l1}')
+    # counting_sort(l1)
+    # print(f'after: {l1}')
+
+    l2 = [1, 3, 4, 2, 4, 1, 10, 41]
+    print(f'before: {l2}')
+    bucket_sort(l2)
+    print(f'after: {l2}')
