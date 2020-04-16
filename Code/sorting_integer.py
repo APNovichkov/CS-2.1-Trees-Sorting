8f800eccf3a86c5470eeb7554456e59a76b447a6
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-15 18:06:53 -0700

    [add] Added time complexity analysis to integer sorts

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -2,19 +2,23 @@
 
 
 def counting_sort(numbers):
-    """Sort given numbers (integers) by counting occurrences of each number,
+    """
+    Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
 
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(2n+range) -> O(n+range), where range is the max-min number.
+    Memory usage: O(range), because we are only creating one c_arr of size 'range'
+    """
 
     max_num = max(numbers)
     min_num = min(numbers)
     c_arr = [0] * (max_num - min_num + 1)
 
+    # O(n)
     for num in numbers:
         c_arr[num - min_num] += 1
 
+    # O(n + range)
     numbers_index = 0
     for index, counts in enumerate(c_arr):
         val = index + min_num
@@ -24,27 +28,34 @@ def counting_sort(numbers):
 
 
 def bucket_sort(numbers, num_buckets=10):
-    """Sort given numbers by distributing into buckets representing subranges,
+    """
+    Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
 
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n), only linear for loops used here
+    Memory usage: O(n), Creating a new list of lists with n number of elements
+    """
 
     max_num = max(numbers)
+    # O(n)
     buckets = [[] for i in range(num_buckets)]
 
+    # O(n)
     for num in numbers:
         b_idx = _get_bucket_index(num, num_buckets, max_num)
         buckets[b_idx].append(num)
         buckets[b_idx].sort()
 
     number_index = 0
+    # O(n)
     for bucket in buckets:
         for num in bucket:
             numbers[number_index] = num
             number_index += 1
 
 def _get_bucket_index(num, num_buckets, max_num):
+    """Return the bucket index from a formula."""
+    
     return (num * num_buckets) // (max_num + 1)
 
 
